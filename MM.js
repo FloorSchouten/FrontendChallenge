b406999f9b77c2d2dccba15d4dd0d3bf8759a1b0
Floor Schouten <corporatecontent@outlook.com>
2021-02-09 14:26:19 +0100

    Changed entering website by using a javascript operated button

--- MM.js
+++ MM.js
@@ -1,9 +1,6 @@
-{/* <button id="Button">M</button>
-function () {
-
-}
-
-    document.getElementById("monkButton").onclick = function () {
-        location.pathname = page2.html;
-    }; */}
+// I wanted to show that I do know some Javascript, in this assignement I just thought the <a> links
+// worked just as well. That's why I made two ways of entering the site: one with html, one with Javascript
+    function goToIntro() {
+        document.getElementById("entering").onclick = location.replace("intro.html")
+      }
 
