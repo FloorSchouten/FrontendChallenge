f811c2c4ecc0d4c6626241f5daf18024cbb3d982
Floor Schouten <corporatecontent@outlook.com>
2021-02-09 13:15:41 +0100

    Finished page 9

--- MM.js
+++ MM.js
@@ -1,32 +1,9 @@
-<button id="Button">M</button>
+{/* <button id="Button">M</button>
 function () {
 
 }
 
     document.getElementById("monkButton").onclick = function () {
         location.pathname = page2.html;
-    };
-// function load(page2) {
-     
-//         document.getElementById("content").innerHTML='<object type="text/html" data="flex.html" ></object>';
-//    }
-
-{/* <button id="MonkButton" class="float-left submit-button" >Home</button>
-
-    document.getElementById("MonkButton").onclick = function () {
-        location.href = "MM.html";
     }; */}
-    // function monkMove() {
-    //     var elem = document.getElementById("monkContainer");
-    //     var pos = 0;
-    //     var id = setInterval(frame, 10);
-    //     function frame() {
-    //       if (pos == 350) {
-    //         clearInterval(id);
-    //       } else {
-    //         pos++;
-    //         elem.style.top = pos + 'px';
-    //         elem.style.left = pos + 'px';
-    //       }
-    //     }
-    //   }
+
